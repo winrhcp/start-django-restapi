2c1a32850b10e7306c9f341074b3730340e457ed
winrhcp <winrhcp@outlook.com>
2021-02-02 00:32:52 +0700

    edit model and add requirement.txt

--- myapi/models.py
+++ myapi/models.py
@@ -9,6 +9,6 @@ class Rapper(models.Model):
 
 class Stock(models.Model):
     name = models.CharField(max_length=100)
-    price = models.DecimalField(max_digits=4, decimal_places=2)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
     def __str__(self):
         return self.name
